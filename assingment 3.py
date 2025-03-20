2b5baaea752a7003198e8508422b546261f866e9
maaz312 <maazhaider312@gmail.com>
2025-03-20 11:37:31 +0500

    assign3

---
+++ assingment 3.py
@@ -0,0 +1,9 @@
+exam_score = 79
+if exam_score == 85:
+    print("exelent")
+elif exam_score >75  <85 :
+    print("good work!")
+elif exam_score >50 <75:
+    print("you passed")
+else:
+    print("you fail")
